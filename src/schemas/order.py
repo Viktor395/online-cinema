851ce1ce84d582c8e02fc2b407a0f3d35a862a74
Viktor Velychkocd <velviktor831@gmail.com>
2026-08-08 11:15:37 +0300

    Add Pydantic schemas for cart and orders

---
+++ src/schemas/order.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+from src.schemas.movie import MovieResponse
+
+class CartResponse(BaseModel):
+    id: int
+    movies: List[MovieResponse]
+
+    class Config:
+        from_attributes = True
+
+class OrderResponse(BaseModel):
+    id: int
+    total_price: float
+    status: str
+    created_at: datetime
+    movies: List[MovieResponse]
+
+    class Config:
+        from_attributes = True
